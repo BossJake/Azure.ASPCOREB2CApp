f587c21a4521fdc23b283e2f49610c3e562540bf
Jake Micua (PC) <jakemicua@gmail.com>
2020-05-30 14:10:27 +1200

    Initial AD B2C asp.net web app.

---
+++ Azure.ASPCOREB2CApp/Startup.cs
@@ -0,0 +1,95 @@
+using System;
+using Azure.ASPCOREB2CApp.Config;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Azure.ASPCOREB2CApp
+{
+    public class Startup
+    {
+        public Startup(IHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds Microsoft Identity platform (AAD B2C v2.0) support to protect this Api
+            // Default can be done here but I have refactored them into the AzureAdB2CAuthenticationBuilderExtensions class
+            // so we don't need to write the whole binding and token validation in here.
+            // Alot cleaner
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddAzureAdB2C(options =>
+            {
+                Configuration.Bind("Authentication:AzureAdB2C", options);
+            })
+            .AddCookie();
+
+            //If is using MVC for for demo purposes only, we will use a React Client App that 
+            //will be done by John.
+            services.AddMvc();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+
+            //We will use cookie to store the identity token for the current logged in user.
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+
+
+        }
+    }
+}
